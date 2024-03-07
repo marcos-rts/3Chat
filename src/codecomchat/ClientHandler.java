5dc018c9b109bed88594261ed41a28e923934a1c
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-03-07 15:26:43 -0300

    configurando servidor

---
+++ src/codecomchat/ClientHandler.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package codecomchat;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author admin_hachiman
+ */
+public class ClientHandler extends Thread{
+    private Socket clientSocket;
+    
+    public ClientHandler(Socket clientSocket){
+        this.clientSocket = clientSocket;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            String message;
+            while ((message = in.readLine()) != null) {
+                System.out.println("Mensagem do cliente: " + message);
+
+                // Envia uma mensagem de resposta de volta para o cliente
+                out.println("Mensagem recebida pelo servidor: " + message);
+            }
+
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/codecomchat/CodeComChat.java
+++ src/codecomchat/CodeComChat.java
@@ -1,20 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package codecomchat;
 
+import java.io.*;
+import java.net.*;
+
+
 /**
- *
- * @author admin_hachiman
+ * @author Marcos Alexnadre Reis Toruato dos Santos
  */
 public class CodeComChat {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        final int PORT = 12345;
+        try (ServerSocket serverSocket = new ServerSocket(PORT)){
+            //aguarda por conexoes de clientes
+            System.out.println("Servidor iniciando. Aguardando conexoes...");
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Novo cliente conectado: " + clientSocket);
+                //lida com a conexao do cliente em uma nova thread
+                new ClientHandler(clientSocket).start();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
     
 }
