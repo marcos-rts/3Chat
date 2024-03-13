b597f8147abf703502b76111e18f9fe7e7160530
Marcos - notebook <marcos.alex1002@gmail.com>
2024-03-13 03:46:13 -0300

    adicionado codigo para autenticar database

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -2,6 +2,10 @@ package server;
 
 import java.io.*;
 import java.net.*;
+import java.sql.*;
+import java.util.Properties;
+
+
 
 /**
  * Esta classe implementa um servidor TCP simples em Java.
@@ -24,12 +28,34 @@ public class Server {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             // Aguardando por conexões de clientes
             System.out.println("Servidor iniciando. Aguardando conexoes...");
+            
+            // Carrega as informações de conexão do arquivo de propriedades
+            Properties properties = loadProperties("db.properties");
+            if (properties == null) {
+                System.err.println("Erro ao carregar as propriedades do banco de dados.");
+                return;
+            }
+            
+            // Estabelece a conexão com o banco de dados
+            Connection connection = getConnection(properties);
+            if (connection == null) {
+                System.err.println("Erro ao conectar ao banco de dados.");
+                return;
+            }
+            
             while (true) {
                 // Aceita uma conexão de cliente
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Novo cliente conectado: " + clientSocket);
                 // Lida com a conexão do cliente em uma nova thread
-                new ClientHandler(clientSocket).start();
+                // Autenticação do cliente com o banco de dados
+                if (autenticarCliente(connection, clientSocket)) {
+                    System.out.println("Cliente autenticado com sucesso.");
+                    new ClientHandler(clientSocket).start();
+                } else {
+                    System.out.println("Falha na autenticação do cliente.");
+                    clientSocket.close();
+                }
             }
         } catch (IOException e) {
             // Exceção ocorreu, imprime o rastreamento da pilha
